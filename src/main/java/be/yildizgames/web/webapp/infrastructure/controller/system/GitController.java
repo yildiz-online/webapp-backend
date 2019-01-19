b84e7403f158003ba90c81c83f3908c97aa731ef
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-01-19 01:07:57 +0100

    [YE-0] Update licence.

--- src/main/java/be/yildizgames/web/webapp/infrastructure/controller/system/GitController.java
+++ src/main/java/be/yildizgames/web/webapp/infrastructure/controller/system/GitController.java
@@ -1,9 +1,9 @@
 /*
  *  This file is part of the Yildiz-Engine project, licenced under the MIT License  (MIT)
  *
- *  Copyright (c) 2018 Grégory Van den Borre
+ *  Copyright (c) 2019 Grégory Van den Borre
  *
- *  More infos available: https://www.yildiz-games.be
+ *  More infos available: https://engine.yildiz-games.be
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  *
